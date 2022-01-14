a8a6cf6385fb0499d97b5cd08635b382feae615e
Christian Holm Christensen <chr100@gmail.com>
2022-01-14 22:20:12 +0100

    refactor app.js functions to have clearer names

--- js/app.js
+++ js/app.js
@@ -1,38 +1,33 @@
 // Potential todos:
 //  - Check if input is a valid number, else display an error message
 //  - Setup testing
-//  - Better styling - flexbox?
-//  - Responisive design
 
+const form = document.querySelector("form");
+const input = form.querySelector("input");
+const button = form.querySelector("button");
+const select = form.querySelector("select");
 
-const form = document.querySelector('form');
-const input = form.querySelector('input');
-const button = form.querySelector('button');
-const select = form.querySelector('select');
-
-const result = document.querySelector('#result');
+const result = document.querySelector("#result");
 let conversionResult;
 
-
-const FtoC = fahrenheit => {
-  return ((fahrenheit - 32) * (5 / 9));
+const FahrenheitToCelcius = (fahrenheit) => {
+  return (fahrenheit - 32) * (5 / 9);
 };
 
-const CtoF = celcius => {
-  return (celcius * (9 / 5)) + 32;
+const CelciusToFahrenheit = (celcius) => {
+  return celcius * (9 / 5) + 32;
 };
 
-
-button.addEventListener('click', event => {
+button.addEventListener("click", (event) => {
   event.preventDefault();
 
   const value = parseInt(input.value);
 
-  if (select.value === 'Fahrenheit') {
-    conversionResult = `${FtoC(value).toFixed(1)} C`;
+  if (select.value === "Fahrenheit") {
+    conversionResult = `${FahrenheitToCelcius(value).toFixed(1)} C`;
   }
-  if (select.value === 'Celcius') {
-    conversionResult = `${CtoF(value).toFixed(1)} F`;
+  if (select.value === "Celcius") {
+    conversionResult = `${CelciusToFahrenheit(value).toFixed(1)} F`;
   }
 
   result.innerText = conversionResult;
